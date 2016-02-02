244a20b5dac433ddf882a682cf4e30de86d24ef9
Rohan Gada <gadarohan17@gmail.com>
2016-02-02 12:10:09 +0530

    cart reorder anomaly workaround

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ var replacehostTo = "http://wohlig.co.in/demo2/";
 
 var ftpString = "U2FsdGVkX1+jcFED/CJbcYNiOJ42eBsjlxqmrcKWSIPH9Sao/4535zPQX5Fa7VYGAHSfkKCXbDpiUfJhkRRijaerS1lJ/k+dSfqsfl45ICkzMTJ7fBNVDj/242ur9ZG4HZDhSe1O/J4vEUboWDRBhg==";
 
-var uploadingFolder = "selfcare2";
+var uploadingFolder = "selfcare";
 var password = "";
 
 //Do not change anything below
