9da3f0c9ece3bc355ff944e31347097477aeb498
sohan1 <shn619@gmail.com>
2016-02-08 12:51:09 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -503,10 +503,13 @@ var countries = [{
         "value": "Zimbabwe"
     }
 ];
-firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider,cfpLoadingBarProvider) {
 
   // for http request with session
   $httpProvider.defaults.withCredentials = true;
+  cfpLoadingBarProvider.includeSpinner = true;
+  cfpLoadingBarProvider.spinnerTemplate = '<div class="loadingcfp"><div class="in-box"><div class="sk-fading-circle"><div class="sk-circle1 sk-circle"></div><div class="sk-circle2 sk-circle"></div><div class="sk-circle3 sk-circle"></div><div class="sk-circle4 sk-circle"></div><div class="sk-circle5 sk-circle"></div><div class="sk-circle6 sk-circle"></div><div class="sk-circle7 sk-circle"></div><div class="sk-circle8 sk-circle"></div><div class="sk-circle9 sk-circle"></div><div class="sk-circle10 sk-circle"></div><div class="sk-circle11 sk-circle"></div><div class="sk-circle12 sk-circle"></div></div>Please wait...</div></div>';
+  cfpLoadingBarProvider.includeBar = false;
 
   $stateProvider
 
