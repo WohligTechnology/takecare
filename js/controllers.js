29f2d42b747eb100b4f5e0d70c38a60b8cdc72e4
Rohan Gada <gadarohan17@gmail.com>
2016-05-10 18:27:48 +0530

    minor fixes

--- js/controllers.js
+++ js/controllers.js
@@ -2597,7 +2597,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
       $scope.calcproduct.calorie = (parseInt(item) * parseFloat($scope.selectedproduct.calorie)).toFixed(0);
       $scope.calcproduct.protein = (parseInt(item) * parseFloat($scope.selectedproduct.protein)).toFixed(1);
       $scope.calcproduct.fat = (parseInt(item) * parseFloat($scope.selectedproduct.fat)).toFixed(1);
-      $scope.calcproduct.carbs = (parseInt(item) * parseFloat($scope.selectedproduct.calorie)).toFixed(1);
+      $scope.calcproduct.carbs = (parseInt(item) * parseFloat($scope.selectedproduct.carbs)).toFixed(1);
       $scope.filter.walking = Math.round($scope.calcproduct.calorie / 4.18);
       $scope.filter.running = Math.round($scope.calcproduct.calorie / 11.09);
       $scope.filter.cycling = Math.round($scope.calcproduct.calorie / 7);
