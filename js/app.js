52e46ac82d30de1711c4a1e115957efd8d9b2abf
Rohan Gada <gadarohan17@gmail.com>
2016-05-12 13:45:55 +0530

    capitalise

--- js/app.js
+++ js/app.js
@@ -420,7 +420,12 @@ firstapp.filter('serverimage', function() {
     }
   };
 });
-
+firstapp.filter('capitalize', function() {
+    return function(input, all) {
+      var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
+      return (!!input) ? input.replace(reg, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
+    }
+  });
 
 var isSlide = false;
 var SlideFunc = {};
